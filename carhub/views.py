da960c1208befeefafac0b282e6500d04f7ff7d6
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-17 22:26:25 +0300

    added the functions to render the pages login and homepage

--- carhub/views.py
+++ carhub/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def admin_dashboard(request):
     return render(request, 'admin_dashboard.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def home(request):
+    return render(request,'home.html')
