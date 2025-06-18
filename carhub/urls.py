f2d700662b3c5082722acd94e4fa00dec04538b0
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-18 16:33:23 +0300

    added the path for logout

--- carhub/urls.py
+++ carhub/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login',views.login,name='login'),
     path('home',views.home,name='home'),
     path('admin_page',views.admin_page,name="admin_page"),
-    path('delete_car/<int:car_id>', views.delete_car, name='delete_car'),
-    path('update_car/<int:car_id>', views.update_car, name='update_car'),
+    path('admin_page/delete_car/<int:car_id>', views.delete_car, name='delete_car'),
+    path('admin_page/update_car/<int:car_id>', views.update_car, name='update_car'),
+    path('logout_user',views.logout_user,name='logout'),
 ]
