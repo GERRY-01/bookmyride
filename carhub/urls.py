7ae32a94baa77a6701205948fd5ebf02589301dd
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-18 15:40:27 +0300

    added the path for deleting cars from the admin side

--- carhub/urls.py
+++ carhub/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('register',views.register,name='register'),
     path('login',views.login,name='login'),
     path('home',views.home,name='home'),
-    path('admin_page',views.admin_page,name="admin_page")
+    path('admin_page',views.admin_page,name="admin_page"),
+    path('delete_car/<int:car_id>', views.delete_car, name='delete_car'),
 ]
