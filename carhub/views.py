c5be29a86feb6b604e4e904b0146e60c648b35d6
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-18 09:52:32 +0300

    added the function for admin login

--- carhub/views.py
+++ carhub/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate,login as auth_login
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def admin_dashboard(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        
+        user = authenticate(request,username=username,password=password)
+        
+        if user is not None and user.is_staff:
+            auth_login(request,user)
+            return redirect("admin_page")
+        else:
+            messages.error(request,"Invalid credentials")
+        
     return render(request, 'admin_dashboard.html')
 
 def register(request):
@@ -13,3 +28,6 @@ def login(request):
 
 def home(request):
     return render(request,'home.html')
+
+def admin_page(request):
+    return render(request,'adminpage.html')
