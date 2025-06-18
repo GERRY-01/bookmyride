76db628a3d0a24204e629a116d0e11e0b8cbfa69
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-18 15:18:44 +0300

    added the path for the admin page

--- carhub/urls.py
+++ carhub/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin-portal-72e9b', views.admin_dashboard, name='admin_dashboard'),
     path('register',views.register,name='register'),
     path('login',views.login,name='login'),
-    path('home',views.home,name='home')
+    path('home',views.home,name='home'),
+    path('admin_page',views.admin_page,name="admin_page")
 ]
