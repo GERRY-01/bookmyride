ceb49d1cc32204b0ccfee1c8b2ba696e6a4e3f3f
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-18 15:23:25 +0300

    created a model for storing the cars data

--- carhub/models.py
+++ carhub/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price_per_day = models.DecimalField(max_digits=8, decimal_places=2)
+    image = models.ImageField(upload_to='cars')
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
