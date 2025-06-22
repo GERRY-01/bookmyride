ef84d990a3b549e20f9abbfb8e0c1769a8d82464
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-22 20:50:54 +0300

    changed the name of the admin login url

--- carhub/urls.py
+++ carhub/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('admin-login-72e9b', views.admin_login, name='admin_dashboard'),
+    path('admin-login-72e9b', views.admin_login, name='admin_login'),
     path('admin-register',views.admin_register,name='admin_register'),
     path('register',views.register,name='register'),
     path('login',views.login,name='login'),
